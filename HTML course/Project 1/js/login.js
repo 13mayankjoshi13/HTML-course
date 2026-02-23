dcdbd717f156701dd4e1554ae4ed96f65334001e
Mayank Joshi <125684136+13mayankjoshi13@users.noreply.github.com>
2026-02-23 23:28:52 +0530

    Delete HTML course directory

--- HTML course/Project 1/js/login.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.body.style.opacity = "0";
-    document.body.style.transition = "opacity 0.6s ease";
-
-    setTimeout(() => {
-        document.body.style.opacity = "1";
-    }, 100);
-});
